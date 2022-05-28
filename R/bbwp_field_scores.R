7b012850ae524ad63b30b4b9ae1e1522ac9a2054
Gerard <gerard.ros@nmi-agro.nl>
2022-05-28 21:50:09 +0200

    add B_LU_BBWP to bbwp_field_score and update impact measurements

--- R/bbwp_field_scores.R
+++ R/bbwp_field_scores.R
@@ -8,6 +8,7 @@
 #' @param A_P_SG (numeric) 
 #' @param B_SLOPE (boolean)
 #' @param B_LU_BRP (integer)
+#' @param B_LU_BBWP (numeric) The BBWP category used for allocation of measures to BBWP crop categories
 #' @param D_WP (numeric) The fraction of the parcel that is surrounded by surface water
 #' @param D_RISK_NGW (numeric) the risk for nitrate leaching to groundwater given field properties
 #' @param D_RISK_NSW (numeric) the risk for nitrate leaching and runoff to surface water given field properties
@@ -20,7 +21,7 @@
 #' @param B_CT_NSW (numeric) the critical target for required reduction in N loss from agriculture (kg N / ha) to reach targets of KRW
 #' @param B_CT_PSW_MAX (numeric) the max critical target for P reduction loss (kg P / ha)
 #' @param B_CT_NSW_MAX (numeric) the max critical target for N reduction loss (kg N / ha)
-#' @param measures (list) the measures planned / done per fields (measurement nr)
+#' @param measures (data.table) the measures planned / done per fields
 #' @param sector (string) a vector with the farm type given the agricultural sector (options: 'melkveehouderij','akkerbouw','vollegrondsgroente','boomteelt','bollen','veehouderij','overig')
 #' 
 #'   
@@ -28,7 +29,7 @@
 #'
 #' @export
 # calculate the opportunities for a set of fields
-bbwp_field_scores <- function(B_SOILTYPE_AGR, B_GWL_CLASS, A_P_SG, B_SLOPE, B_LU_BRP, 
+bbwp_field_scores <- function(B_SOILTYPE_AGR, B_GWL_CLASS, A_P_SG, B_SLOPE, B_LU_BRP, B_LU_BBWP,
                               M_DRAIN, D_WP, D_RISK_NGW, D_RISK_NSW, D_RISK_PSW, D_RISK_NUE, D_RISK_WB,
                               B_GWP, B_AREA_DROUGHT, B_CT_PSW, B_CT_NSW, 
                               B_CT_PSW_MAX = 0.5, B_CT_NSW_MAX = 5.0, measures, sector){
@@ -40,7 +41,7 @@ bbwp_field_scores <- function(B_SOILTYPE_AGR, B_GWL_CLASS, A_P_SG, B_SLOPE, B_LU
   
   # check length of the inputs
   arg.length <- max(length(B_SOILTYPE_AGR),length(B_GWL_CLASS), length(A_P_SG),
-                    length(B_SLOPE), length(B_LU_BRP), length(M_DRAIN),length(D_WP),
+                    length(B_SLOPE), length(B_LU_BRP), length(B_LU_BBWP),length(M_DRAIN),length(D_WP),
                     length(D_RISK_NGW),length(D_RISK_NSW),length(D_RISK_PSW),length(D_RISK_NUE),
                     length(D_RISK_WB),length(B_GWP),length(B_AREA_DROUGHT),length(B_CT_PSW),
                     length(B_CT_NSW))
@@ -49,8 +50,9 @@ bbwp_field_scores <- function(B_SOILTYPE_AGR, B_GWL_CLASS, A_P_SG, B_SLOPE, B_LU
   checkmate::assert_subset(B_SOILTYPE_AGR, choices = c('duinzand','dekzand','zeeklei','rivierklei','maasklei',
                                                        'dalgrond','moerige_klei','veen','loess'))
   checkmate::assert_numeric(A_P_SG, lower = 0, upper = 120, len = arg.length)
-  checkmate::assert_logical(B_SLOPE, len = arg.length)
+  checkmate::assert_numeric(B_SLOPE, len = arg.length)
   checkmate::assert_integerish(B_LU_BRP, lower = 0, len = arg.length)
+  checkmate::assert_integerish(B_LU_BBWP, lower = 0, upper = 9,len = arg.length)
   checkmate::assert_logical(M_DRAIN,len = arg.length)
   checkmate::assert_numeric(D_WP, lower = 0, upper = 1, len = arg.length)
   checkmate::assert_numeric(D_RISK_NGW, lower = 0, upper = 1, len = arg.length)
@@ -62,90 +64,98 @@ bbwp_field_scores <- function(B_SOILTYPE_AGR, B_GWL_CLASS, A_P_SG, B_SLOPE, B_LU
   checkmate::assert_logical(B_AREA_DROUGHT,len = arg.length)
   checkmate::assert_numeric(B_CT_PSW, lower = 0, upper = 50, len = arg.length)
   checkmate::assert_numeric(B_CT_NSW, lower = 0, upper = 100, len = arg.length)
-  checkmate::assert_list(measures)
+
+  # load, check and update the measures database
+  dt.measures <- bbwp_check_meas(measures,eco = FALSE,score = TRUE)
   
   # collect data in one data.table
-  dt <- data.table(
-    id = 1:arg.length,
-    B_SOILTYPE_AGR = B_SOILTYPE_AGR,
-    B_GWL_CLASS = B_GWL_CLASS,
-    A_P_SG = A_P_SG,
-    B_SLOPE = B_SLOPE,
-    B_LU_BRP = B_LU_BRP,
-    M_DRAIN = M_DRAIN,
-    D_WP = D_WP,
-    D_RISK_NGW = D_RISK_NGW,
-    D_RISK_NSW = D_RISK_NSW,
-    D_RISK_PSW = D_RISK_PSW,
-    D_RISK_NUE = D_RISK_NUE,
-    D_RISK_WB = D_RISK_WB,
-    B_GWP = B_GWP,
-    B_AREA_DROUGHT = B_AREA_DROUGHT,
-    B_CT_PSW = B_CT_PSW,
-    B_CT_NSW = B_CT_NSW,
-    B_CT_PSW_MAX = B_CT_PSW_MAX,
-    B_CT_NSW_MAX = B_CT_NSW_MAX
-  )
-  
-  # add list of measures
+  dt <- data.table(id = 1:arg.length,
+                    B_SOILTYPE_AGR = B_SOILTYPE_AGR,
+                    B_GWL_CLASS = B_GWL_CLASS,
+                    A_P_SG = A_P_SG,
+                    B_SLOPE = B_SLOPE,
+                    B_LU_BRP = B_LU_BRP,
+                    B_LU_BBWP = B_LU_BBWP,
+                    M_DRAIN = M_DRAIN,
+                    D_WP = D_WP,
+                    D_RISK_NGW = D_RISK_NGW,
+                    D_RISK_NSW = D_RISK_NSW,
+                    D_RISK_PSW = D_RISK_PSW,
+                    D_RISK_NUE = D_RISK_NUE,
+                    D_RISK_WB = D_RISK_WB,
+                    B_GWP = B_GWP,
+                    B_AREA_DROUGHT = B_AREA_DROUGHT,
+                    B_CT_PSW = B_CT_PSW,
+                    B_CT_NSW = B_CT_NSW,
+                    B_CT_PSW_MAX = B_CT_PSW_MAX,
+                    B_CT_NSW_MAX = B_CT_NSW_MAX
+                  )
   
   # calculate correction factors, depending on regional targets
   
-  # correction when field is in a ground water protection zone
-  dt[,cfngw := fifelse(B_GWP, 1, 0.5)]
-  
-  # correction when field is in a region with high water deficiency risks
-  dt[,cfwb := fifelse(B_AREA_DROUGHT, 1, 0.5)]
-  
-  # correction when field is in a region with high target for N load reduction surface water
-  dt[,cfnsw := B_CT_NSW / B_CT_NSW_MAX]
-  
-  # correction when field is in a region with high target for P load reduction surface water
-  dt[,cfpsw := B_CT_PSW / B_CT_PSW_MAX]
-  
-  # replace to max critical limit when no information is ready
-  dt[is.na(cfpsw), cfpsw := 1]
-  dt[is.na(cfnsw), cfnsw := 1]
-  
-  # correction for need for increased nutrient use efficiency
-  dt[, cfnue := 0.5]
+    # correction when field is in a ground water protection zone
+    dt[,cfngw := fifelse(B_GWP, 1, 0.5)]
+    
+    # correction when field is in a region with high water deficiency risks
+    dt[,cfwb := fifelse(B_AREA_DROUGHT, 1, 0.5)]
+    
+    # correction when field is in a region with high target for N load reduction surface water
+    dt[,cfnsw := B_CT_NSW / B_CT_NSW_MAX]
+    
+    # correction when field is in a region with high target for P load reduction surface water
+    dt[,cfpsw := B_CT_PSW / B_CT_PSW_MAX]
+    
+    # replace to max critical limit when no information is ready
+    dt[is.na(cfpsw), cfpsw := 1]
+    dt[is.na(cfnsw), cfnsw := 1]
+    
+    # correction for need for increased nutrient use efficiency
+    dt[, cfnue := 0.5]
   
   # calculate the individual opportunity indexes
-  dt[,D_OPI_NGW := evaluate_logistic(D_RISK_NGW, b=7, x0=0.5 - cfngw * .1, v=.7)]
-  dt[,D_OPI_NSW := evaluate_logistic(D_RISK_NSW, b=7, x0=0.5 - cfnsw * .1, v=.7)]
-  dt[,D_OPI_PSW := evaluate_logistic(D_RISK_PSW, b=7, x0=0.5 - cfpsw * .1, v=.7)]
-  dt[,D_OPI_NUE := evaluate_logistic(D_RISK_NUE, b=7, x0=0.5 - cfnue * .1, v=.7)]
-  dt[,D_OPI_WB := evaluate_logistic(D_RISK_WB, b=7, x0=0.5 - cfwb * .1, v=.7)]
+  dt[,D_OPI_NGW := OBIC::evaluate_logistic(D_RISK_NGW, b=7, x0=0.5 - cfngw * .1, v=.7)]
+  dt[,D_OPI_NSW := OBIC::evaluate_logistic(D_RISK_NSW, b=7, x0=0.5 - cfnsw * .1, v=.7)]
+  dt[,D_OPI_PSW := OBIC::evaluate_logistic(D_RISK_PSW, b=7, x0=0.5 - cfpsw * .1, v=.7)]
+  dt[,D_OPI_NUE := OBIC::evaluate_logistic(D_RISK_NUE, b=7, x0=0.5 - cfnue * .1, v=.7)]
+  dt[,D_OPI_WB := OBIC::evaluate_logistic(D_RISK_WB, b=7, x0=0.5 - cfwb * .1, v=.7)]
   
   # calculate the change in opportunity indexes given the measures taken
   
-  # column names for impact of measures on the five indexes (do not change order)
-  mcols <- c('D_MEAS_NGW', 'D_MEAS_NSW', 'D_MEAS_PSW', 'D_MEAS_NUE', 'D_MEAS_WB', 'D_MEAS_TOT')
-  
-  # estimate these indexes
-  nr.measures <- length(Filter(function(x) dim(x)[1] > 0, measures))
-  if (nr.measures > 0 ) {
-    dt[,c(mcols) := bbwp_meas_score(
-      B_SOILTYPE_AGR = dt$B_SOILTYPE_AGR,
-      B_GWL_CLASS = dt$B_GWL_CLASS,
-      A_P_SG = dt$A_P_SG,
-      B_SLOPE = dt$B_SLOPE,
-      B_LU_BRP = dt$B_LU_BRP,
-      M_DRAIN = dt$M_DRAIN,
-      D_WP = dt$D_WP,
-      D_OPI_NGW = dt$D_OPI_NGW,
-      D_OPI_NSW = dt$D_OPI_NSW,
-      D_OPI_PSW = dt$D_OPI_PSW,
-      D_OPI_NUE = dt$D_OPI_NUE,
-      D_OPI_WB = dt$D_OPI_WB,
-      measures = measures,
-      sector = sector
-    )]
-  } else {
-    dt[, c('D_MEAS_NGW','D_MEAS_NSW','D_MEAS_PSW','D_MEAS_NUE','D_MEAS_WB','D_MEAS_TOT') := 0]
-  }
-  
-  
+    # column names for impact of measures on the five indexes (do not change order)
+    mcols <- c('D_MEAS_NGW', 'D_MEAS_NSW', 'D_MEAS_PSW', 'D_MEAS_NUE', 'D_MEAS_WB', 'D_MEAS_TOT')
+    
+    # calculate the total score per indicator 
+    if(nrow(dt.measures) > 0){
+      
+      # calculate
+      dt.meas.impact <- bbwp_meas_score(B_SOILTYPE_AGR = dt$B_SOILTYPE_AGR, 
+                                        B_LU_BRP = dt$B_LU_BRP,
+                                        B_LU_BBWP = dt$B_LU_BBWP,
+                                        B_GWL_CLASS = dt$B_GWL_CLASS,
+                                        A_P_SG = dt$A_P_SG,
+                                        B_SLOPE = dt$B_SLOPE,
+                                        M_DRAIN = dt$M_DRAIN,
+                                        D_WP = dt$D_WP,
+                                        D_OPI_NGW = dt$D_OPI_NGW,
+                                        D_OPI_NSW = dt$D_OPI_NSW,
+                                        D_OPI_PSW = dt$D_OPI_PSW,
+                                        D_OPI_NUE = dt$D_OPI_NUE,
+                                        D_OPI_WB = dt$D_OPI_WB,
+                                        measures = measures, 
+                                        sector = sector)
+      
+      # merge with dt
+      dt <- merge(dt,dt.meas.impact,by='id')
+      
+      # set NA to zero
+      dt[,c(mcols) := lapply(.SD,function(x) fifelse(is.na(x),0,x)),.SDcols = mcols]
+      
+    } else {
+      
+      # set impact of management to zero when no measures are applied
+      dt[,c(mcols) := list(0,0,0,0,0,0)]
+    }
+    
   # update the field score with measures
   dt[,D_OPI_NGW := pmax(0,1 - pmax(0, D_OPI_NGW - D_MEAS_NGW))]
   dt[,D_OPI_NSW := pmax(0,1 - pmax(0, D_OPI_NSW - D_MEAS_NSW))]
