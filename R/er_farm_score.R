87e0c58c82691d6eab913d24dd5a62d98cc2d9ad
Astrid Berndsen <astrid.berndsen@nmi-agro.nl>
2022-08-24 11:32:32 +0200

    Added calculation of the integrative total score since total score should be a value between 0 and 100. Changed upper limit to 500 for input S_ER_TOT.

--- R/er_farm_score.R
+++ R/er_farm_score.R
@@ -19,6 +19,7 @@ er_farm_score <- function(S_ER_TOT,S_ER_SOIL,S_ER_WATER,S_ER_CLIMATE,S_ER_BIODIV
                           S_ER_REWARD, B_AREA){
   
   code = value_min = value_max = NULL
+  cfSOIL = cfWAT = cfCLIM = cfBIO = cfLAND = S_ERTOT_WEIGHTED = NULL
   
   # Load bbwp_parms
   bbwp_parms <- BBWPC::bbwp_parms
@@ -28,7 +29,7 @@ er_farm_score <- function(S_ER_TOT,S_ER_SOIL,S_ER_WATER,S_ER_CLIMATE,S_ER_BIODIV
                     length(S_ER_BIODIVERSITY),length(S_ER_LANDSCAPE),length(B_AREA))
   
   # check inputs
-  checkmate::assert_numeric(S_ER_TOT, lower = 0, upper = 100, len = arg.length)
+  checkmate::assert_numeric(S_ER_TOT, lower = 0, upper = 500, len = arg.length)
   checkmate::assert_numeric(S_ER_SOIL, lower = 0, upper = 100, len = arg.length)
   checkmate::assert_numeric(S_ER_WATER, lower = 0, upper = 100, len = arg.length)
   checkmate::assert_numeric(S_ER_CLIMATE, lower = 0, upper = 100, len = arg.length)
@@ -49,14 +50,34 @@ er_farm_score <- function(S_ER_TOT,S_ER_SOIL,S_ER_WATER,S_ER_CLIMATE,S_ER_BIODIV
                    B_AREA = B_AREA
   )
   
+  # calculate the integrative opportunity index (risk times impact) (since the S_ER_TOT is the absolute total instead of the weighted total)
+  
+  # weigh the importance given "distance to target"
+  dt[,cfSOIL := wf(S_ER_SOIL, type="score")]
+  dt[,cfWAT := wf(S_ER_WATER, type="score")]
+  dt[,cfCLIM := wf(S_ER_CLIMATE, type="score")]
+  dt[,cfBIO := wf(S_ER_BIODIVERSITY, type="score")]
+  dt[,cfLAND := wf(S_ER_LANDSCAPE, type="score")]
+  
+  # weighted mean
+  dt[,S_ER_TOT_WEIGHTED := (S_ER_SOIL * cfSOIL + S_ER_WATER * cfWAT + S_ER_CLIMATE * cfCLIM + S_ER_BIODIVERSITY * cfBIO + S_ER_LANDSCAPE * cfLAND) / 
+       (cfSOIL + cfWAT + cfCLIM + cfBIO + cfLAND)]
+  
+  # keep relevant columns 
+  cols <- c('id','S_ER_SOIL','S_ER_WATER','S_ER_CLIMATE','S_ER_BIODIVERSITY','S_ER_LANDSCAPE','S_ER_TOT_WEIGHTED','S_ER_REWARD')
+  dt[, mget(cols)]
+  
+  # rename the opportunity index in order that the output name of variable that will be returned stays the same
+  setnames(dt,'S_ER_TOT_WEIGHTED','S_ER_TOT')
+  
   # columns with the score of the opportunity indexes
   cols <- c('S_ER_SOIL','S_ER_WATER','S_ER_CLIMATE','S_ER_BIODIVERSITY','S_ER_LANDSCAPE','S_ER_TOT','S_ER_REWARD')
   
-  # calculate area weigthed sum of the field indices
+  # calculate area weighted sum of the field indices
   dt <- dt[,lapply(.SD, stats::weighted.mean, w = B_AREA), .SDcols = cols]
   
   # Round the values
-  dt<- dt[, lapply(.SD, round, digits = 0)]
+  dt <- dt[, lapply(.SD, round, digits = 0)]
   
   # return output
   return(dt)
