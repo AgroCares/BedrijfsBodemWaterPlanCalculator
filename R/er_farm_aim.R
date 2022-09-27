d60fcdba4df4441e3499ec47b8d1b37001b8d4cf
Gerard <gerard.ros@nmi-agro.nl>
2022-09-27 14:22:58 +0200

    convert output thresholds to a single list, and set NA to NULL

--- R/er_farm_aim.R
+++ R/er_farm_aim.R
@@ -148,10 +148,18 @@ er_farm_aim <- function(B_SOILTYPE_AGR, B_AREA, medalscore = "gold", farmscore =
     setnames(out.threshold.silver,paste0(colnames(out.threshold.silver),'_silver'))
     setnames(out.threshold.gold,paste0(colnames(out.threshold.gold),'_gold'))
     
+    # convert to list
+    out.threshold.bronze <- as.list(out.threshold.bronze)
+    out.threshold.silver <- as.list(out.threshold.silver)
+    out.threshold.gold <- as.list(out.threshold.gold)
+    
     # combine in one output object
-    out.threshold <- data.table(out.threshold.bronze,
-                                out.threshold.silver,
-                                out.threshold.gold)
+    out.threshold <- c(as.list(out.threshold.bronze),
+                       as.list(out.threshold.silver),
+                       as.list(out.threshold.gold))
+    
+    # set missing values to NULL
+    out.threshold[is.na(out.threshold)] <- list(NULL)
     
     # set output object with target and thresholds
     out.tgt <- out.threshold
