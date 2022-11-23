e790e348d62563d0e5b191b1b66d3fbd8ec427c3
Astrid Berndsen <astrid.berndsen@nmi-agro.nl>
2022-11-23 17:18:56 +0100

    Make function er_pdf which makes a table of measures that generate scores

--- R/er_pdf.R
+++ R/er_pdf.R
@@ -1,30 +1,29 @@
-#' Calculate the crop rotation based total score for five opportunity indicators
+#' Make table of measures that generated scores
 #'
-#' Estimate the actual contribution of crop rotation given aims for soil quality, water quality, climate, biodiversity and landscape
+#' Present each measure that generated scores on field and/or farm level in table format with scores for soil quality, water quality, climate, biodiversity and landscape given soil type.
 #'
-#' @param meas_scores (boolean) is the pdf needed for function er_meas_scores
-#' @param croprotation (boolean) is the pdf needed for function er_croprotation
+#' @param measurescores (boolean) is the pdf requested in function er_meas_scores
+#' @param croprotation (boolean) is the pdf requested in function er_croprotation
 #' @param dt.field.measures (datatable) field measures
-#' @param dt.farm.measures (datatable) field measures
-
+#' @param dt.farm.measures (datatable) farm measures
+#' @param B_AREA (numeric) the area of the field (m2) 
 #'
 #' @import data.table
-#' @import stats
 #'
 #' @export
 # calculate the opportunities for a set of fields
-er_pdf <- function(croprotation,meas_scores,dt.field.measures,dt.farm.measures){
+er_pdf <- function(croprotation,measurescores,dt.field.measures,dt.farm.measures,B_AREA){
   
   # add visual bindings
-  eco_id = type = fr_area = id = er_urgency = NULL
-
+  . = dt1 = pdf.meas.field = pdf.field.meas.name = pdf.field.measures = level = NULL
+  pdf.farm.meas.name = pdf.farm.measures = pdf.field.tot = area_farm = pdf.tot = NULL
+  total = euro_ha = euro_farm = bbwp_id = bbwp_measures = B_AREA_tot = NULL
   
+  # get internal measures table
+  bbwp_measures <- as.data.table(BBWPC::bbwp_measures)
   
-  
-  
-  # should pdf function be used in er_meas_scores? 
-  #dt.field.measures is dt2 in er_meas_scores
-  if(meas_scores = TRUE){
+  # create table with field measures taken and corresponding scores from er_meas_score 
+  if(measurescores == TRUE){
     
     # get applied measures that have a score or reward
     pdf.meas.field <- dt.field.measures[total > 0 | euro_ha > 0 | euro_farm >0,]
@@ -36,7 +35,7 @@ er_pdf <- function(croprotation,meas_scores,dt.field.measures,dt.farm.measures){
     dt1 <- bbwp_measures[, c("summary","bbwp_id")]
     
     # merge measure summary with applied measures
-    pdf.meas.field <- merge(pdf.meas.field,dt1, on = "bbwp_id")
+    pdf.meas.field <- merge(pdf.meas.field,dt1, by = "bbwp_id")
     
     # convert area to ha
     pdf.meas.field <- pdf.meas.field[, B_AREA := B_AREA/10000]
@@ -53,17 +52,16 @@ er_pdf <- function(croprotation,meas_scores,dt.field.measures,dt.farm.measures){
       
     # arrange table to right format
     pdf.meas.field <- pdf.meas.field[, bbwp_id := NULL]
-    setcolorder(pdf.meas.field, c("summary"))
+    pdf.meas.field[, level := "field"]
+    setcolorder(pdf.meas.field, c("level","summary"))
     
-  }
+    # set output
+    out <- pdf.meas.field
+  } 
     
   
-  
-  # should pdf function be used in er_croprotation? 
-  #dt.field.measures is dt2 in er_croprotation
-  #dt.farm.measures is dt4 in er_croprotation
-  
-  if(croprotation = TRUE){
+  # create table with field and farm measures taken and corresponding scores from er_croprotation
+  if(croprotation == TRUE){
     
     # get measures applied on field level
     pdf.field.meas.name <- dt.field.measures[total>0 | euro_ha > 0, c("id","bbwp_id")]
@@ -89,10 +87,11 @@ er_pdf <- function(croprotation,meas_scores,dt.field.measures,dt.farm.measures){
       
       # calculate weighted mean of the scores
       pdf.field.measures <- pdf.field.measures[,lapply(.SD,weighted.mean,w = B_AREA), by = c("summary","bbwp_id","B_AREA_tot"),.SDcols = cols]
-      
+     
     # arrange table to right format
     pdf.field.measures <- pdf.field.measures[, c('bbwp_id') := NULL]
-    setcolorder(pdf.field.measures, c("summary"))
+    pdf.field.measures[, level := "field"]
+    setcolorder(pdf.field.measures, c("level","summary"))
     
     # get measures applied on farm level
     pdf.farm.meas.name <- dt.farm.measures[total>0 | euro_farm > 0 | euro_ha > 0, c("bbwp_id")]
@@ -103,28 +102,26 @@ er_pdf <- function(croprotation,meas_scores,dt.field.measures,dt.farm.measures){
     # get applied measures and corresponding scores (in score per farm)
     pdf.farm.measures <- merge(pdf.farm.meas.name,dt.farm.measures[!is.na(bbwp_id), c("bbwp_id","climate","soil","water","landscape","biodiversity","total")], by = c('bbwp_id'))
     
+    # get total farm area in ha
+    area_farm = (sum(B_AREA)/10000)
+    
     # convert farm scores to score per ha
-    pdf.farm.measures <- pdf.field.farm.score[, c(cols):= lapply(.SD, function (x) x / (dt.farm$area_farm/10000)), .SDcols = cols]
+    pdf.farm.measures <- pdf.farm.measures[, c(cols):= lapply(.SD, function (x) x / (area_farm)), .SDcols = cols]
     
     # arrange table to right format
-    pdf.farm.measures <- pdf.farm.measures[, c('bbwp_id') := NULL]
-    setcolorder(pdf.farm.measures, c("summary"))
+    pdf.farm.measures <- pdf.farm.measures[, c('bbwp_id') := NULL][, B_AREA_tot := area_farm]
+    pdf.farm.measures[, level := "farm"]
+    setcolorder(pdf.farm.measures, c("level","summary"))
+    pdf.farm.measures[, c(cols) := round(.SD,1), .SDcols = cols]
+   
+    # bind farm and field scores
+    pdf.farm.field <- rbind(pdf.field.measures,pdf.farm.measures)
+    
+    # set output
+    out <- pdf.meas.field
   }
   
-  
-  # hier een tabel van  maken 
-  
-  pdf.meas.field
-  
-  pdf.field.measures
-  
-  pdf.farm.measures
-  
-  
-  
-  
-  
-  # return table, with 
+  # return table
   return(out)
   
 }
