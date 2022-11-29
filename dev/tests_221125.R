094618f2d6e8531fa17cb61ea18172d9fe430a92
Astrid Berndsen <astrid.berndsen@nmi-agro.nl>
2022-11-29 11:56:40 +0100

    Update tests

--- dev/tests_221125.R
+++ dev/tests_221125.R
@@ -1,7 +1,7 @@
 # tests B&N 25 November 2022
 
 # LOCATION TO STORE CSV TESTS
-loc <- 'C:/Astrid/BBWP_ecoregeling bestanden/'
+loc <- 'C:/Astrid/B&N_tests_25nov22/'
 
 ### test scenarios on request for BenN ###
 # get internal table with measures
@@ -11,6 +11,16 @@ dt.measures <- dt.measures[!is.na(eco_id)]
 # measures 
 measures <- NULL
 measures$bbwp_status <- NULL
+
+
+## bedrijf 1
+#measures taken 
+measures <- rbind(data.table(id = 2, dt.measures[grepl('EB4B',eco_id)]),
+                  data.table(id = 3, dt.measures[grepl('EB13A',eco_id)]),
+                  data.table(id = 4, dt.measures[grepl('EB13B',eco_id)]))
+
+measures$bbwp_status <- 'none'
+
 
 
 # uitgangspunten bedrijf 1
@@ -50,7 +60,8 @@ bedrijfsscore1 <- ecoregeling(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
                               sector = sector,
                               farmscore = farmscore,
                               medalscore = medalscore,
-                              output = 'scores'
+                              output = 'scores',
+                              pdf = F
 )
 
 # get farmscores
@@ -58,6 +69,7 @@ farmscores <- as.vector(unlist(bedrijfsscore1$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore1$farm)))[1:10]
 out1 <- data.table(thema = scorenames,score = farmscores)
 out1a <- as.data.table(bedrijfsscore1$fields)
+out1a <- out1a[, !c("s_er_tot")]
 
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
@@ -66,11 +78,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf1.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf1.csv"))
 
 # write scores
-fwrite(out1,paste0(loc, "scoresbedrijf1.csv"))
-fwrite(out1a,paste0(loc, "scoresveldenbedrijf1.csv"))
+write.csv2(out1,paste0(loc, "scoresbedrijf1.csv"))
+write.csv2(out1a,paste0(loc, "scoresveldenbedrijf1.csv"))
 
 
 
@@ -79,11 +91,10 @@ fwrite(out1a,paste0(loc, "scoresveldenbedrijf1.csv"))
 
 
 ## uitgangspunten bedrijf 2A
-measures <- rbind(data.table(id = 1, dt.measures[grepl('EB13A',eco_id)]),
-                  data.table(id = 3, dt.measures[grepl('EB13B',eco_id)]),
-                  data.table(id = 5, dt.measures[grepl('EB13A',eco_id)]))
+measures <- rbind(data.table(id = 1, dt.measures[grepl('EB20',eco_id)]),
+                  data.table(id = 4, dt.measures[grepl('EB7A|EB11A',eco_id)]))
 
-measures$bbwp_status <- 'hello check'
+measures$bbwp_status <- 'none'
 
 B_SOILTYPE_AGR = c('veen','zeeklei','zeeklei','dekzand','dekzand')
 B_GWL_CLASS = rep('GtIII',5)
@@ -122,7 +133,8 @@ bedrijfsscore2 <- ecoregeling(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
                               sector = sector,
                               farmscore = farmscore,
                               medalscore = medalscore,
-                              output = 'scores'
+                              output = 'scores',
+                              pdf = F
 )
 
 # get farmscores
@@ -130,6 +142,7 @@ farmscores <- as.vector(unlist(bedrijfsscore2$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore2$farm)))[1:10]
 out2 <- data.table(thema = scorenames,score = farmscores)
 out2a <- as.data.table(bedrijfsscore2$fields)
+out2a <- out2a[, !c("s_er_tot")]
 
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
@@ -138,11 +151,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf2A.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf2A.csv"))
 
 # write scores
-fwrite(out2,paste0(loc, "scoresbedrijf2A.csv"))
-fwrite(out2a,paste0(loc, "scoresveldenbedrijf2A.csv"))
+write.csv2(out2,paste0(loc, "scoresbedrijf2A.csv"))
+write.csv2(out2a,paste0(loc, "scoresveldenbedrijf2A.csv"))
 
 
 
@@ -203,6 +216,8 @@ farmscores <- as.vector(unlist(bedrijfsscore2$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore2$farm)))[1:10]
 out2 <- data.table(thema = scorenames,score = farmscores)
 out2a <- as.data.table(bedrijfsscore2$fields)
+out2a <- out2a[, !c("s_er_tot")]
+
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
                    B_AREA = B_AREA, 
@@ -210,11 +225,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf2B.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf2B.csv"))
 
 # write scores
-fwrite(out2,paste0(loc, "scoresbedrijf2B.csv"))
-fwrite(out2a,paste0(loc, "scoresveldenbedrijf2B.csv"))
+write.csv2(out2,paste0(loc, "scoresbedrijf2B.csv"))
+write.csv2(out2a,paste0(loc, "scoresveldenbedrijf2B.csv"))
 
 
 
@@ -223,23 +238,24 @@ fwrite(out2a,paste0(loc, "scoresveldenbedrijf2B.csv"))
 
 
 ## uitgangspunten bedrijf 3A
-measures <- rbind(data.table(id = 1, dt.measures[grepl('EG1C',eco_id)]))
+measures <- rbind(data.table(id = 2, dt.measures[grepl('EG10A|EG1C|EG5B',eco_id)]),
+                  data.table(id = 3, dt.measures[grepl('EB12|EB15',eco_id)]))
 
-measures$bbwp_status <- 'hello check'
+measures$bbwp_status <- 'none'
 
 B_SOILTYPE_AGR = c('dekzand','dekzand','zeeklei','loess','dekzand')
 B_GWL_CLASS = rep('GtIII',5)
 A_P_SG = rep(12,5)
 B_SLOPE_DEGREE = rep(1.5,5)
 B_AER_CBS = rep('LG04',5)
-B_LU_BBWP = c('natuur','gras_tijdelijk','rooivrucht','gras_permanent','randensloot')
-B_LU_BRP = c(2621,266,1909,265,3721)
-B_LU_ARABLE_ER = c(T,F,T,F,T)
-B_LU_PRODUCTIVE_ER = c(F,T,T,T,F)
+B_LU_BBWP = c('natuur','gras_tijdelijk','rooivrucht','gras_permanent','gras_tijdelijk')
+B_LU_BRP = c(2621,266,1909,265,266)
+B_LU_ARABLE_ER = c(T,F,T,F,F)
+B_LU_PRODUCTIVE_ER = c(F,T,T,T,T)
 B_LU_CULTIVATED_ER = c(F,T,T,T,T)
 M_DRAIN = rep(T,5)
 D_SA_W = rep(0.5,5)
-B_AREA = c(1000,20000,30000,40000,1000)
+B_AREA = c(1000,20000,30000,40000,20000)
 farmscore = 100
 medalscore = "gold"
 measures = measures
@@ -265,7 +281,8 @@ bedrijfsscore3 <- ecoregeling(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
                               sector = sector,
                               farmscore = farmscore,
                               medalscore = medalscore,
-                              output = 'scores'
+                              output = 'scores',
+                              pdf=T
 )
 
 # get farmscores
@@ -273,6 +290,7 @@ farmscores <- as.vector(unlist(bedrijfsscore3$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore3$farm)))[1:10]
 out3 <- data.table(thema = scorenames,score = farmscores)
 out3a <- as.data.table(bedrijfsscore3$fields)
+out3a <- out3a[, !c("s_er_tot")]
 
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
@@ -281,11 +299,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf3A.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf3A.csv"))
 
 # write scores
-fwrite(out3,paste0(loc, "scoresbedrijf3A.csv"))
-fwrite(out3a,paste0(loc, "scoresveldenbedrijf3A.csv"))
+write.csv2(out3,paste0(loc, "scoresbedrijf3A.csv"))
+write.csv2(out3a,paste0(loc, "scoresveldenbedrijf3A.csv"))
 
 
 
@@ -344,6 +362,7 @@ farmscores <- as.vector(unlist(bedrijfsscore3$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore3$farm)))[1:10]
 out3 <- data.table(thema = scorenames,score = farmscores)
 out3a <- as.data.table(bedrijfsscore3$fields)
+out3a <- out3a[, !c("s_er_tot")]
 
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
@@ -352,11 +371,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf3B.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf3B.csv"))
 
 # write scores
-fwrite(out3,paste0(loc, "scoresbedrijf3B.csv"))
-fwrite(out3a,paste0(loc, "scoresveldenbedrijf3B.csv"))
+write.csv2(out3,paste0(loc, "scoresbedrijf3B.csv"))
+write.csv2(out3a,paste0(loc, "scoresveldenbedrijf3B.csv"))
 
 
 
@@ -371,11 +390,16 @@ fwrite(out3a,paste0(loc, "scoresveldenbedrijf3B.csv"))
 
 
 ## uitgangspunten bedrijf 4A
-measures <- rbind(data.table(id = 5, dt.measures[grepl('EG14',eco_id)]),
-                  data.table(id = 6, dt.measures[grepl('EG13',eco_id)]))
+measures <- rbind(data.table(id = 1, dt.measures[grepl('EG8A',eco_id)]),
+                  data.table(id = 2, dt.measures[grepl('EG7B',eco_id)]),
+                  data.table(id = 4, dt.measures[grepl('EG16C',eco_id)]),
+                  data.table(id = 5, dt.measures[grepl('EG14',eco_id)]),
+                  data.table(id = 6, dt.measures[grepl('EG13',eco_id)]),
+                  data.table(id = 7, dt.measures[grepl('EG14',eco_id)])
+                  )
 
 
-measures$bbwp_status <- 'hello check'
+measures$bbwp_status <- 'none'
 
 
 B_SOILTYPE_AGR = c('veen','veen','zeeklei','zeeklei','dekzand','dekzand','dekzand')
@@ -390,7 +414,7 @@ B_LU_PRODUCTIVE_ER = c(F,T,T,T,F,F,F)
 B_LU_CULTIVATED_ER = c(F,T,T,T,F,F,F)
 M_DRAIN = rep(T,7)
 D_SA_W = rep(0.7,7)
-B_AREA = c(10000,20000,30000,40000,1000,2500,3000)
+B_AREA = c(10000,20000,30000,40000,1000,2000,3000)
 farmscore = 100
 medalscore = "gold"
 measures = measures
@@ -416,7 +440,8 @@ bedrijfsscore4 <- ecoregeling(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
                               sector = sector,
                               farmscore = farmscore,
                               medalscore = medalscore,
-                              output = 'scores'
+                              output = 'scores',
+                              pdf = F
 )
 
 # get farmscores
@@ -424,6 +449,9 @@ farmscores <- as.vector(unlist(bedrijfsscore4$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore4$farm)))[1:10]
 out4 <- data.table(thema = scorenames,score = farmscores)
 out4a <- as.data.table(bedrijfsscore4$fields)
+out4a <- out4a[, !c("s_er_tot")]
+
+
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
                    B_AREA = B_AREA, 
@@ -431,11 +459,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf4A.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf4A.csv"))
 
 # write scores
-fwrite(out4,paste0(loc, "scoresbedrijf4A.csv"))
-fwrite(out4a,paste0(loc, "scoresveldenbedrijf4A.csv"))
+write.csv2(out4,paste0(loc, "scoresbedrijf4A.csv"))
+write.csv2(out4a,paste0(loc, "scoresveldenbedrijf4A.csv"))
 
 
 
@@ -492,6 +520,8 @@ farmscores <- as.vector(unlist(bedrijfsscore4$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore4$farm)))[1:10]
 out4 <- data.table(thema = scorenames,score = farmscores)
 out4a <- as.data.table(bedrijfsscore4$fields)
+out4a <- out4a[, !c("s_er_tot")]
+
 
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
@@ -500,11 +530,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf4B.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf4B.csv"))
 
 # write scores
-fwrite(out4,paste0(loc, "scoresbedrijf4B.csv"))
-fwrite(out4a,paste0(loc, "scoresveldenbedrijf4B.csv"))
+write.csv2(out4,paste0(loc, "scoresbedrijf4B.csv"))
+write.csv2(out4a,paste0(loc, "scoresveldenbedrijf4B.csv"))
 
 
 
@@ -565,6 +595,8 @@ farmscores <- as.vector(unlist(bedrijfsscore4$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore4$farm)))[1:10]
 out4 <- data.table(thema = scorenames,score = farmscores)
 out4a <- as.data.table(bedrijfsscore4$fields)
+out4a <- out4a[, !c("s_er_tot")]
+
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
                    B_AREA = B_AREA, 
@@ -572,11 +604,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf4C.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf4C.csv"))
 
 # write scores
-fwrite(out4,paste0(loc, "scoresbedrijf4C.csv"))
-fwrite(out4a,paste0(loc, "scoresveldenbedrijf4C.csv"))
+write.csv2(out4,paste0(loc, "scoresbedrijf4C.csv"))
+write.csv2(out4a,paste0(loc, "scoresveldenbedrijf4C.csv"))
 
 
 
@@ -632,6 +664,8 @@ farmscores <- as.vector(unlist(bedrijfsscore5$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore5$farm)))[1:10]
 out5 <- data.table(thema = scorenames,score = farmscores)
 out5a <- as.data.table(bedrijfsscore5$fields)
+out5a <- out5a[, !c("s_er_tot")]
+
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
                    B_AREA = B_AREA, 
@@ -639,11 +673,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf5.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf5.csv"))
 
 # write scores
-fwrite(out5,paste0(loc, "scoresbedrijf5.csv"))
-fwrite(out5a,paste0(loc, "scoresveldenbedrijf5.csv"))
+write.csv2(out5,paste0(loc, "scoresbedrijf5.csv"))
+write.csv2(out5a,paste0(loc, "scoresveldenbedrijf5.csv"))
 
 
 
@@ -699,6 +733,7 @@ farmscores <- as.vector(unlist(bedrijfsscore_6$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore_6$farm)))[1:10]
 out6 <- data.table(thema = scorenames,score = farmscores)
 out6a <- as.data.table(bedrijfsscore_6$fields)
+out6a <- out6a[, !c("s_er_tot")]
 
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
@@ -707,11 +742,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf6.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf6.csv"))
 
 # write scores
-fwrite(out6,paste0(loc, "scoresbedrijf6.csv"))
-fwrite(out6a,paste0(loc, "scoresveldenbedrijf6.csv"))
+write.csv2(out6,paste0(loc, "scoresbedrijf6.csv"))
+write.csv2(out6a,paste0(loc, "scoresveldenbedrijf6.csv"))
 
 
 
@@ -768,6 +803,7 @@ farmscores <- as.vector(unlist(bedrijfsscore$farm))[1:10]
 scorenames <- as.vector(unlist(names(bedrijfsscore$farm)))[1:10]
 out7 <- data.table(thema = scorenames,score = farmscores)
 out7a <- as.data.table(bedrijfsscore$fields)
+out7a <- out7a[, !c("s_er_tot")]
 
 # get aim
 aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR, 
@@ -776,11 +812,11 @@ aim <- er_farm_aim(B_SOILTYPE_AGR = B_SOILTYPE_AGR,
 aimscore <- as.vector(unlist(aim))
 aimname <- as.vector(unlist(names(aim)))[!grepl("landscape_br|landscape_si",names(aim))]
 aim <- data.table(threshold = aimname,score = aimscore)
-fwrite(aim,paste0(loc, "aimbedrijf6.csv"))
+write.csv2(aim,paste0(loc, "aimbedrijf6.csv"))
 
 # write scores
-fwrite(out7,paste0(loc, "scoresbedrijf6.csv"))
-fwrite(out7a,paste0(loc, "scoresveldenbedrijf6.csv"))
+write.csv2(out7,paste0(loc, "scoresbedrijf6.csv"))
+write.csv2(out7a,paste0(loc, "scoresveldenbedrijf6.csv"))
 
 
 
