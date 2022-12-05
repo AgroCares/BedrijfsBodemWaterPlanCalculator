604530b569ba45a78be9fbd91fd467ebaf146a57
Gerard <gerard.ros@nmi-agro.nl>
2022-12-05 23:19:21 +0100

    comment default input test-er

--- tests/testthat/test-er.R
+++ tests/testthat/test-er.R
@@ -1,25 +1,26 @@
 require(testthat)
 
   # # default input for testing
-  B_SOILTYPE_AGR = c('dekzand', 'loess', 'rivierklei')
-  B_GWL_CLASS = c('GtIII', 'GtI', 'GtV')
-  B_AER_CBS = c('LG05','LG14','LG02')
-  A_P_SG = c(0.4, 0.8, 1)
-  B_SLOPE_DEGREE = c(1.5,4,1.5)
-  B_AER_CBS = c('LG05','LG14','LG02')
-  B_LU_BBWP = rep('gras_permanent',3)
-  B_LU_BRP = rep(265,3)
-  B_LU_ARABLE_ER = c(F,F,F)
-  B_LU_PRODUCTIVE_ER = c(T,T,T)
-  B_LU_CULTIVATED_ER = c(T,T,T)
-  M_DRAIN = c(TRUE, FALSE, TRUE)
-  D_SA_W = c(0, 0.5, 1)
-  B_AREA = c(1000000,800000,25000)
-  measures = NULL
-  farmscore = 100
-  sector = c('dairy', 'arable')
-  output = 'scores'
-  medalscore = 'gold'
+  # B_SOILTYPE_AGR = c('dekzand', 'loess', 'rivierklei')
+  # B_GWL_CLASS = c('GtIII', 'GtI', 'GtV')
+  # B_AER_CBS = c('LG05','LG14','LG02')
+  # A_P_SG = c(0.4, 0.8, 1)
+  # B_SLOPE_DEGREE = c(1.5,4,1.5)
+  # B_AER_CBS = c('LG05','LG14','LG02')
+  # B_LU_BBWP = rep('gras_permanent',3)
+  # B_LU_BRP = rep(265,3)
+  # B_LU_ARABLE_ER = c(F,F,F)
+  # B_LU_PRODUCTIVE_ER = c(T,T,T)
+  # B_LU_CULTIVATED_ER = c(T,T,T)
+  # M_DRAIN = c(TRUE, FALSE, TRUE)
+  # D_SA_W = c(0, 0.5, 1)
+  # B_AREA = c(1000000,800000,25000)
+  # measures = NULL
+  # farmscore = 100
+  # sector = c('dairy', 'arable')
+  # output = 'scores'
+  # medalscore = 'gold'
+  # pdf = FALSE
 
 # run example 1 without any measures taken
 test <- ecoregeling(B_SOILTYPE_AGR = c('dekzand', 'loess', 'rivierklei'),
@@ -142,7 +143,8 @@ test <- ecoregeling(B_SOILTYPE_AGR = c('dekzand', 'loess', 'rivierklei'),
                     farmscore = 100,
                     measures = measures,
                     sector = c('dairy', 'arable'),
-                    output = 'scores'
+                    medalscore = 'gold',
+                    output = 'scores',pdf=FALSE
 )
 
   # run tests on format and output values
