4b1613abbabf0b159df1147fe478d8336667f5c6
Brent <72964508+BrentWHR@users.noreply.github.com>
2025-07-04 11:48:23 +0200

    in bbwp_measures restore effect_wb and add effect_gw

--- dev/ppr_tables_bbwp.R
+++ dev/ppr_tables_bbwp.R
@@ -9,7 +9,7 @@ require(data.table);library(usethis)
 bbwp_measures <- fread('dev/bbwp_measures.csv', encoding = 'UTF-8')
 
 # save measures as bbwp table
-use_data(bbwp_measures, overwrite = TRUE)
+# use_data(bbwp_measures, overwrite = TRUE)
 
 
 # -- prepare measures table (with landscape category)-----
@@ -19,7 +19,22 @@ use_data(bbwp_measures, overwrite = TRUE)
 # The table was made in Github Repository "NMI-DATA_scripts"
 # (https://github.com/AgroCares/NMI-DATA_scripts/blob/main/watersysteem/bbwp_hydrologische_module/bbwp_hydro_meas.R)
 # This is the table userd for >= version 2.3.0
-bbwp_measures <- fread('dev/bbwp_measures2.csv', encoding = 'UTF-8')
+bbwp_measures2 <- fread('dev/bbwp_measures2.csv', encoding = 'UTF-8')
+
+# add groundwater effect
+setnames(bbwp_measures2, 'effect_wb', 'effect_gw')
+bbwp_measures2 <- merge(bbwp_measures2, bbwp_measures[,.(bbwp_id, effect_wb)], by = 'bbwp_id', all.x = TRUE)
+
+# estimate effect_wb of measures > version 2.3.0 based on similarity in bootlijst
+bbwp_measures[boot22_id %in% c(bbwp_measures2[is.na(effect_wb), .(boot22_id)]), .(bbwp_id, nr_mok, boot22_id, eco_id, effect_wb)]
+boot_wb_ids <- unique(bbwp_measures2[is.na(effect_wb), .(boot22_id)])
+boot_wb_ids <- merge(boot_wb_ids, unique(bbwp_measures[,.(boot22_id, effect_wb)]), by = 'boot22_id')
+setnames(boot_wb_ids, 'effect_wb', 'temp_effect_wb')
+bbwp_measures2 <- merge(bbwp_measures2, boot_wb_ids, by = 'boot22_id', all.x = TRUE)
+bbwp_measures2[is.na(effect_wb), effect_wb := temp_effect_wb]
+bbwp_measures2[,temp_effect_wb := NULL]
+bbwp_measures <- copy(bbwp_measures2)
+rm(bbwp_measures2)
 
 # Overwrite bbwp measure table
 use_data(bbwp_measures, overwrite = TRUE)
